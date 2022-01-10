31c9b778d456b91ca4797f756d676003a1e78fe8
l bates childs <labchilds@gmail.com>
2022-01-10 18:29:24 -0500

    added Player methods and tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -20,4 +20,27 @@ test('create a Player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+// test Player obj method getStats() which returns data
+test("gets the Player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    // checks if stats obj has potions, health, strength, agility properties
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+// test Player obj has method getInventory(), which return inventory and should be arr
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+    // check that getInventory method returns array (should have values on creation)
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    // set inventory prop to empty arr to check false return
+    player.inventory = [];
+    // check that empty arr in inventory prop returns false when getInventory method is called
+    expect(player.getInventory()).toEqual(false);
 })
